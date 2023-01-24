3a26154ce51d8bb083002b3007bd0fae9d6ef7a9
AlexLowczyk <alex.lowczyk@gmail.com>
2023-01-23 20:53:11 -0500

    Update teams.ts
    Switched what needed to be switched

--- frontend/src/app/teams.ts
+++ frontend/src/app/teams.ts
@@ -1,137 +1,67 @@
-export class Teams {
-    AutoHuman: Number;
-    AutoPickUp: Number;
-    AutoScoredHigh: Number;
-    AutoScoredLow: Number;
-    AutoStartPosID: Number;
-    AutoSummary: String;
-    CanClimb: Number;
-    ClimbHeightID: Number;
-    ClimbPosition: Number;
-    ClimbStrategy: String;
-    ClimbTime: Number;
-    DriveTypeID: Number;
-    GearRatio: String;
-    HasAuto: Number;
-    IntakeType: Number;
-    LanguageID: Number;
-    MaxBallCapacity: Number;
-    MotorTypeID: Number;
-    MoveBonus: Number;
-    NumDriveMotors: Number;
-    NumGearSpeed: Number;
-    NumWheels: Number;
-    Pneumatics: Number;
-    Preload: Number;
-    RobotHeight: Number;
-    RobotLength: Number;
-    RobotWeight: Number;
-    RobotWidth: Number;
-    Speed: Number;
-    Team: Number;
-    TeamCity: String;
-    TeamCountry: String;
-    TeamLocation: String;
-    TeamName: String;
-    TeamStateProv: String;
-    TeleBallsScoredHigh: Number;
-    TeleBallsScoredLow: Number
-    TeleDefense: Number;
-    TeleDefenseEvade: Number;
-    TeleDefenseStrat: String;
-    TeleShootWhileDrive: String;
-    TeleSortCargo: Number;
-    TeleStrategy: String;
-    WheelTypeID: Number;
-constructor(
-    AutoHuman: Number,
-    AutoPickUp: Number,
-    AutoScoredHigh: Number,
-    AutoScoredLow: Number,
-    AutoStartPosID: Number,
-    AutoSummary: String,
-    CanClimb: Number,
-    ClimbHeightID: Number,
-    ClimbPosition: Number,
-    ClimbStrategy: String,
-    ClimbTime: Number,
-    DriveTypeID: Number,
-    GearRatio: String,
-    HasAuto: Number,
-    IntakeType: Number,
-    LanguageID: Number,
-    MaxBallCapacity: Number,
-    MotorTypeID: Number,
-    MoveBonus: Number,
-    NumDriveMotors: Number,
-    NumGearSpeed: Number,
-    NumWheels: Number,
-    Pneumatics: Number,
-    Preload: Number,
-    RobotHeight: Number,
-    RobotLength: Number,
-    RobotWeight: Number,
-    RobotWidth: Number,
-    Speed: Number,
-    Team: Number,
-    TeamCity: String,
-    TeamCountry: String,
-    TeamLocation: String,
-    TeamName: String,
-    TeamStateProv: String,
-    TeleBallsScoredHigh: Number,
-    TeleBallsScoredLow: Number,
-    TeleDefense: Number,
-    TeleDefenseEvade: Number,
-    TeleDefenseStrat: String,
-    TeleShootWhileDrive: String,
-    TeleSortCargo: Number,
-    TeleStrategy: String,
-    WheelTypeID: Number,){
+import { G } from "@angular/cdk/keycodes";
 
-    this.AutoHuman = AutoHuman,
-    this.AutoPickUp = AutoPickUp;
-    this.AutoScoredHigh = AutoScoredHigh;
-    this.AutoScoredLow = AutoScoredLow;
-    this.AutoStartPosID = AutoStartPosID;
-    this.AutoSummary = AutoSummary;
-    this.CanClimb = CanClimb;
-    this.ClimbHeightID = ClimbHeightID;
-    this.ClimbPosition = ClimbPosition;
-    this.ClimbStrategy = ClimbStrategy;
-    this.ClimbTime = ClimbTime;
-    this.DriveTypeID = DriveTypeID;
-    this.GearRatio = GearRatio;
-    this.HasAuto = HasAuto;
-    this.IntakeType = IntakeType;
-    this.LanguageID = LanguageID;
-    this.MaxBallCapacity = MaxBallCapacity;
-    this.MotorTypeID = MotorTypeID;
-    this.MoveBonus = MoveBonus;
-    this.NumDriveMotors = NumDriveMotors;
-    this.NumGearSpeed = NumGearSpeed;
-    this.NumWheels = NumWheels;
-    this.Pneumatics = Pneumatics;
-    this.Preload = Preload;
-    this.RobotHeight = RobotHeight;
-    this.RobotLength = RobotLength;
-    this.RobotWeight = RobotWeight;
-    this.RobotWidth = RobotWidth;
-    this.Speed = Speed;
-    this.Team = Team;
-    this.TeamCity = TeamCity;
-    this.TeamCountry = TeamCountry;
-    this.TeamLocation = TeamLocation;
-    this.TeamName = TeamName;
-    this.TeamStateProv = TeamStateProv;
-    this.TeleBallsScoredHigh = TeleBallsScoredHigh;
-    this.TeleBallsScoredLow = TeleBallsScoredLow
-    this.TeleDefense = TeleDefense;
-    this.TeleDefenseEvade = TeleDefenseEvade;
-    this.TeleDefenseStrat = TeleDefenseStrat;
-    this.TeleShootWhileDrive = TeleShootWhileDrive;
-    this.TeleSortCargo = TeleSortCargo;
-    this.TeleStrategy = TeleStrategy;
-    this.WheelTypeID = WheelTypeID;
+export class Teams {
+    buddyClimb: Number;
+    buildComments: Number;
+    buildType: Number;
+    buildQuality: Number;
+    centerGravity: Number;
+    driveBase: Number;
+    driveTypeID: Number;
+    electricalQuality: Number;
+    electricalComments: Number;
+    eventID: Number;
+    generalComments: Number;
+    height: Number;
+    intakeType: Number;
+    manupulator: Number;
+    motorType: Number;
+    robotLength: Number;
+    robotdurability: Number;
+   team: String;
+    width: Number;
+    
+constructor(
+    buddyClimb: Number,
+    buildComments: Number,
+    buildType: Number,
+    buildQuality: Number,
+    centerGravity: Number,
+    driveBase: Number,
+    driveTypeID: Number,
+    electricalQuality: Number,
+    electricalComments: Number,
+   eventID: Number,
+   generalComments: Number,
+   height: Number,
+    intakeType: Number,
+    manupulator: Number,
+    motorType: Number,
+    robotLength: Number,
+    robotdurability: Number,
+    team: String,
+    width: Number,
+   
+    ){
+this.buddyClimb = buddyClimb;
+this.buildComments = buildComments;
+this.buildType = buildType;
+this.buildQuality = buildQuality;
+this.centerGravity = centerGravity;
+   this.driveBase = driveBase
+    this.driveTypeID = driveTypeID;
+    this.electricalQuality = electricalQuality;
+    this.electricalComments = electricalComments;
+    this.eventID = eventID;
+    this.intakeType = intakeType;
+    this.generalComments = generalComments;
+    this.height = height;
+    this.motorType = motorType;
+    this.manupulator = manupulator;
+    this.robotLength = robotLength;
+    this.robotdurability= robotdurability;
+    this.team = team
+    this.width = width;
+    
 }}
 
