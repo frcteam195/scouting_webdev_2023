7d6bd7c5b8e0676e318d2fbadd69dc93ef909b1b
Alex Fenwick <alexfenladwickner@gmail.com>
2023-02-28 20:45:49 -0500

    updated robot graphic to pull info

--- frontend/src/app/modules/robot-graphic/robot-graphic.component.ts
+++ frontend/src/app/modules/robot-graphic/robot-graphic.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { CEA } from '../../CEA';
 @Component({
   selector: 'app-robot-graphic',
   templateUrl: './robot-graphic.component.html',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RobotGraphicComponent implements OnInit {
 
-  constructor() { }
+  @Input() selectedTeam: string;
+
+  apiAnalysis: CEA[] = [];
+  title: String;
+  titleShow = true;
+
+  constructor(private apiService: ApiService) {
+    this.apiAnalysis = [];
+    this.title = "Title";
+    this.selectedTeam = "";
+
+    // Update the filter whenever the inputting data changes
+    this.apiService.CEAReplay.subscribe(analysis => {
+      this.apiAnalysis = analysis;
+    });
+
+  }
 
   ngOnInit(): void {
+    console.log("Value passed to robo graphic: " + this.selectedTeam)
   }
 
 }
