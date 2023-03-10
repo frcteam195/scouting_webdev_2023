8ac6df3ee1c92e051be85b0a8056043e84f0e807
ddrex <44214161+ddrex@users.noreply.github.com>
2023-03-09 20:45:25 -0500

    Backend change to upload files.  Modified gray bars in match report

--- backend/backend.py
+++ backend/backend.py
@@ -10,7 +10,7 @@ import os
 from flask import redirect, url_for, flash
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/media'
+UPLOAD_FOLDER = 'media'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app=Flask(__name__)
@@ -36,7 +36,8 @@ mysql = MySQL(app)
 
 @app.route("/")
 def hello():
-    return "Hello Worlds!"
+    # return "Hello Worlds!"
+    return redirect("https://www.team195.com")
 
 @app.route("/harish/")
 def hello2():
@@ -801,38 +802,24 @@ def get_currteam():
 
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+@app.route('/upload/<team>', methods=['GET','POST'])
+def upload_file(team):
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            # return redirect(request.url)
-            return redirect(request.url)            
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # return redirect(url_for('download_file', name=filename))
-            return redirect(request.url)  
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    file = request.files['robotImage']
+
+    print("File Upload: [" + team + "] [" + str(file) +"]")
+
+    # print("Old File: " + file.filename)
+
+    filepart = os.path.splitext(file.filename)
+    extension = filepart[1]
+    filename = "frc" + team + extension
+
+    # print("New File: " + filename)
+
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+    return '0'
 
 
 
